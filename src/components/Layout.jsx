7e898c4e3a3edd50f28aa099aab4375c1fe59d34
2437920530@qq.com <2437920530@qq.com>
2020-09-28 14:57:03 +0800

    添加底部标签栏

---
+++ src/components/Layout.jsx
@@ -0,0 +1,117 @@
+import { TabBar } from 'antd-mobile';
+import React, { Component } from 'react';
+
+// import cookbook from 'assert/'
+
+
+export default class Layout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'cookbook',
+      hidden: false,
+      fullScreen: true,
+    };
+  }
+
+
+  render() {
+    return (
+      <div style={this.state.fullScreen ? { position: 'fixed', height: '100%', width: '100%', top: 0 } : { height: 400 }}>
+        <TabBar
+          unselectedTintColor="#666"
+          tintColor="#000"
+          barTintColor="white"
+          hidden={this.state.hidden}
+        >
+          <TabBar.Item
+            title="美食大全"
+            key="cookbook"
+            icon={<div style={{
+              width: '22px',
+              height: '22px',
+              background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  21px 21px no-repeat' }}
+            />
+            }
+            selectedIcon={<div style={{
+              width: '22px',
+              height: '22px',
+              background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  21px 21px no-repeat' }}
+            />
+            }
+            selected={this.state.selectedTab === 'cookbook'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'cookbook',
+              });
+            }}
+          >
+          </TabBar.Item>
+          <TabBar.Item
+            icon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://gw.alipayobjects.com/zos/rmsportal/BTSsmHkPsQSPTktcXyTV.svg) center center /  21px 21px no-repeat' }}
+              />
+            }
+            selectedIcon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://gw.alipayobjects.com/zos/rmsportal/ekLecvKBnRazVLXbWOnE.svg) center center /  21px 21px no-repeat' }}
+              />
+            }
+            title="分类"
+            key="category"
+            selected={this.state.selectedTab === 'category'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'category',
+              });
+            }}
+          >
+          </TabBar.Item>
+          <TabBar.Item
+            icon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://zos.alipayobjects.com/rmsportal/psUFoAMjkCcjqtUCNPxB.svg) center center /  21px 21px no-repeat' }}
+              />
+            }
+            selectedIcon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://zos.alipayobjects.com/rmsportal/IIRLrXXrFAhXVdhMWgUI.svg) center center /  21px 21px no-repeat' }}
+              />
+            }
+            title="美食地图"
+            key="map"
+            selected={this.state.selectedTab === 'map'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'map',
+              });
+            }}
+          >
+          </TabBar.Item>
+          <TabBar.Item
+            icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
+            selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg' }}
+            title="更多"
+            key="more"
+            selected={this.state.selectedTab === 'more'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'more',
+              });
+            }}
+          >
+          </TabBar.Item>
+        </TabBar>
+      </div>
+    );
+  }
+}
